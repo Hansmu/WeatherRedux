6a20e6e38f595d5a2e9a0e25b9e2c1a010167080
Hans Miikael Uuspõld <hans.miikael.uuspold@icefire.ee>
2016-05-23 10:27:09 +0300

    Added an Axios and a request with it.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = "19066c9219d5f45440e07a1661b3890d";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeatherData(cityName) {
+    const url = `${ROOT_URL}$q?${cityName},est`
+    const request = axios.get(url);
+
+    //Use library called Axios to make query requests, instead of using jQuery.
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
